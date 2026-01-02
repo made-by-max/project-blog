4aed7a647128b3aaec2738bb764a865634de08ef
made-by-max <madebymax.dev@gmail.com>
2026-01-02 10:34:19 -0800

    added metadata to homepage and blog posts

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,8 +2,21 @@ import React from "react";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import BlogHero from "@/components/BlogHero";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { cache } from "react";
 
 import styles from "./postSlug.module.css";
+
+
+
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params;
+  const { frontmatter } = await loadBlogPost(postSlug);
+  return {
+    title: `${frontmatter.title}`,
+    description: `${frontmatter.abstract}`,
+  };
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
@@ -15,7 +28,9 @@ async function BlogPost({ params }) {
         title={frontmatter.title}
         publishedOn={frontmatter.publishedOn}
       />
-      <MDXRemote source={content} />
+      <div className={styles.page}>
+        <MDXRemote source={content} />
+      </div>
     </article>
   );
 }
